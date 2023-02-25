57c335afc740a54f465dfedab0af8df35a5c7aea
ArturYusupov <YusupovArturrr@gmail.com>
2023-02-25 11:21:02 +0300

    fragment_signin.xml was added

---
+++ app/src/main/java/com/iusupov/testemobile/view/signin/SignInFragment.kt
@@ -0,0 +1,7 @@
+package com.iusupov.testemobile.view.signin
+
+import com.iusupov.testemobile.databinding.FragmentSigninBinding
+import com.iusupov.testemobile.utils.BaseFragment
+
+class SignInFragment : BaseFragment<FragmentSigninBinding>(FragmentSigninBinding::inflate) {
+}
