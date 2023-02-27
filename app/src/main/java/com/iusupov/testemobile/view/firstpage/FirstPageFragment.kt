aab46a6cb0ba26ac4ea7433cd2ccd177f4577c96
ArturYusupov <YusupovArturrr@gmail.com>
2023-02-27 19:43:48 +0300

    item_latest.xml was added

---
+++ app/src/main/java/com/iusupov/testemobile/view/firstpage/FirstPageFragment.kt
@@ -0,0 +1,7 @@
+package com.iusupov.testemobile.view.firstpage
+
+import com.iusupov.testemobile.databinding.FragmentFirstPageBinding
+import com.iusupov.testemobile.utils.BaseFragment
+
+class FirstPageFragment : BaseFragment<FragmentFirstPageBinding>(FragmentFirstPageBinding::inflate) {
+}
